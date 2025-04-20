bd7558b77cff46004f0d92b25a332f9268d5449b
Youhyunwoo <dbgusdn0922@naver.com>
2025-04-20 12:26:32 +0900

    Input Controller 추가

--- Assets/02_Scripts/Controller/TileInteractionController.cs
+++ Assets/02_Scripts/Controller/TileInteractionController.cs
@@ -1,25 +1,69 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Afterlife.Controller
 {
     public class TileInteractionController : MonoBehaviour
     {
-        [SerializeField] View.TerrainTileIndicator terrainTileIndicator;
+        [SerializeField] InputController inputController;
 
         Model.Player player;
         Model.Stage stage;
 
+        Coroutine interactRoutine;
+        bool isPointerDown;
+
         void OnEnable()
         {
-            terrainTileIndicator.OnInteractedEvent += OnInteract;
+            inputController.OnPointerDownEvent += OnPointerDown;
+            inputController.OnPointerUpEvent += OnPointerUp;
         }
 
         void OnDisable()
         {
-            terrainTileIndicator.OnInteractedEvent -= OnInteract;
+            inputController.OnPointerDownEvent -= OnPointerDown;
+            inputController.OnPointerUpEvent -= OnPointerUp;
         }
 
-        void OnInteract(Vector2Int location)
+        void OnPointerDown(Vector2Int location)
+        {
+            isPointerDown = true;
+            if (interactRoutine != null)
+            {
+                StopCoroutine(interactRoutine);
+            }
+            interactRoutine = StartCoroutine(InteractRoutine(location));
+        }
+
+        IEnumerator InteractRoutine(Vector2Int location)
+        {
+            InteractByLocation(location);
+
+            var waitTime = new WaitForSeconds(player.AttackSpeed / 1f);
+            var playerAttackSpeed = player.AttackSpeed;
+            while (isPointerDown)
+            {
+                if (playerAttackSpeed != player.AttackSpeed)
+                {
+                    playerAttackSpeed = player.AttackSpeed;
+                    waitTime = new WaitForSeconds(player.AttackSpeed / 1f);
+                }
+                yield return waitTime;
+                InteractByLocation(inputController.TileIndex);
+            }
+        }
+
+        void OnPointerUp(Vector2Int location)
+        {
+            isPointerDown = false;
+            if (interactRoutine != null)
+            {
+                StopCoroutine(interactRoutine);
+            }
+            interactRoutine = null;
+        }
+
+        void InteractByLocation(Vector2Int location)
         {
             if (!stage.Map.Field.InBounds(location)) { return; }
             var tileTransform = stage.Map.Field.Get(location);
