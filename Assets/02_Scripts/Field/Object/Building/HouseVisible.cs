4e7a90538db2da911a74985e4a317fc5ec432d65
Youhyunwoo <dbgusdn0922@naver.com>
2026-03-27 17:44:33 +0900

    기존 레거시 시민과 오브젝트 코드 제거

--- Assets/02_Scripts/Field/Object/Building/HouseVisible.cs
+++ Assets/02_Scripts/Field/Object/Building/HouseVisible.cs
@@ -1,23 +1,8 @@
-using Afterlife.Dev.Agent;
 using UnityEngine;
 
 namespace Afterlife.Dev.Field
 {
     public class HouseVisible : BuildingVisible
     {
-        private void OnTriggerEnter2D(Collider2D collision)
-        {
-            if (collision.CompareTag("Player"))
-            {
-                if (collision.TryGetComponent(out Citizen citizen))
-                {
-                    if (citizen._isObjectObtained)
-                    {
-                        citizen.ObtainObject();
-                        Debug.Log("목재 +1 획득!");
-                    }
-                }
-            }
-        }
     }
 }
